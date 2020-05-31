991dd683fbf009a623f89ad97dde5b4c3df0bf46
kkeat <sp.dd.keat@gmail.com>
2020-05-31 22:36:31 +0800

    used GET for all requests, due to crsf_token

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function(){
+    $id=$_GET["id"];
+    $pwd=$_GET["pwd"];
+    $count = DB::select("select count(*) from users_sample where login = '?' and pwd= '?'", [$id,$pwd]);
+    if ($count > 0){
+        return response(0,200)->header('Content-Type', 'application/json');
+    }
+});
+
+Route::get('/register', function(){
+    $id=$_GET["id"];
+    $pwd=$_GET["pwd"];
+    $count = DB::select("select count(*) from users_sample where login = '?'", [$id]);
+    if ($count > 0){
+        return response(0,200)->header('Content-Type', 'application/json');
+    }else{
+    $results = DB::insert("INSERT INTO users_sample (login,pwd) VALUES (?,?)",[$id,$pwd]);
+     return response($results, 200)
+                  ->header('Content-Type', 'application/json');
+    }
+});
