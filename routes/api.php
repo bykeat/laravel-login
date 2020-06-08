98623ce412a87f20b6f21f45e05f8686656d1eee
kkeat <sp.dd.keat@gmail.com>
2020-06-08 16:03:26 +0800

    code clean up

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Services\Booking;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,14 +18,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/login', function () {
+Route::get('/login', function (Booking $booking) {
     try {
         $id = $_GET["id"];
         $pwd = $_GET["pwd"];
         $hash = hash("sha256", $pwd);
 
-        $result = DB::table('users')->where('email', $id)->where('password', $hash)->get();
-
+        $result = $booking . getUserByIdAndPassword($id, $hash);
+        //DB::table('users')->where('email', $id)->where('password', $hash)->get();
         if ($result->count() > 0) {
             $name = $result[0]->name;
             return response()->json(['status' => 200, "message" => "Welcome, " . $name]);
@@ -37,73 +37,41 @@ Route::get('/login', function () {
     }
 });
 
-Route::get('/register', function () {
+Route::get('/register', function (Booking $booking) {
     try {
         $id = $_GET["id"];
         $pwd = $_GET["pwd"];
         $hash = hash("sha256", $pwd);
         $name = $_GET["name"];
 
-        $data = DB::table('users')->where('email', [$id])->get();
+        $data = $booking->getUserById($id);
 
         if ($data->count() > 0) {
             return response()->json(['status' => 500, "message" => "User exists."]);
         } else {
-            $results = DB::table('users')->insert(['email' => $id, 'password' => $hash, 'name' => $name]);
+            $results = $booking->addUser($id, $hash, $name);
             return response()->json(['status' => 200, "message" => "You may login with your email."]);
         }
     } catch (Exception $e) {
+        error_log($e);
         return response()->json(['status' => 500]);
     }
 });
 
-Route::get('/confirm_booking', function () {
+Route::get('/confirm_booking', function (Booking $booking) {
     try {
 
         $booking_id = $_GET["booking_id"];
         $fcm_token = $_GET["fcm_token"];
-
         $status = 1;
-        $response = 1;
-        // DB::table("booking")
-        //     ->where("booking_id", $booking_id)
-        //     ->update(["status" => $status]);
+        $response = $booking->updateBookingStatus($booking_id, $status);
+
         if ($response === 1) {
-            error_log("Booking confirmed!");
-            $json_data = array
-                (
-                'to' => $fcm_token,
-                'notification' => array(
-                    'body' => 'Your booking ' . $booking_id . ' has confirmed.',
-                    'title' => 'Booking confirmed',
-                ),
-            );
-
-            $url = 'https://fcm.googleapis.com/fcm/send';
-            $server_key = env('FCM_KEY');
-            $headers = array(
-                'Content-Type:application/json',
-                'Authorization:key=' . $server_key,
-            );
-            //CURL request to route notification to FCM connection server (provided by Google)
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($json_data));
-            $result = curl_exec($ch);
-            if ($result === false) {
-                error_log("SENT" . $result);
-                die('FCM Send Error: ' . curl_error($ch));
-            } else {
-                error_log("ERROR" . $result);
-            }
-            curl_close($ch);
+            $body = 'Your booking ' . $booking_id . ' has confirmed.';
+            $title = 'Your booking is confirmed';
+            $json_data = $booking->createMessage($fcm_token, $title, $body);
+            $booking->sendNotification($json_data);
         }
-
         return response()->json(['status' => 200, 'message' => "Booking confirmed."]);
     } catch (Exception $e) {
         error_log($e);
@@ -111,24 +79,20 @@ Route::get('/confirm_booking', function () {
     }
 });
 
-Route::get('/cancel_booking', function () {
+Route::get('/cancel_booking', function (Booking $booking) {
     try {
         $booking_id = $_GET["booking_id"];
         $status = -1;
-        $result = DB::table("booking")
-            ->where("booking_id", $booking_id)
-            ->update(["status" => $status]);
+        $result = $booking->updateBookingStatus($booking_id, $status);
         return response()->json(['status' => 200, 'message' => "Booking cancelled."]);
     } catch (Exception $e) {
         return response()->json(['status' => 500, 'message' => "Booking cancellation error."]);
     }
 });
 
-Route::get('/make_booking', function () {
+Route::get('/make_booking', function (Booking $booking) {
     try {
-
         date_default_timezone_set(env("TIMEZONE"));
-
         try {
             $fcm_token = $_GET["fcm_token"];
             $booking_type = $_GET["booking_type"];
@@ -142,48 +106,18 @@ Route::get('/make_booking', function () {
             return response()->json(['status' => 500, 'message' => "Unable to calculate distance due to invalid input."]);
         }
 
-        if ($booking_type === "pre") {
-            $current_hour = date('H', strtotime($pickup_time));
-        } else {
-            $current_hour = date('H', time());
-        }
-
-        $dayFareHour = env("DAY_HOUR");
-        $nightFareHour = env("NIGHT_HOUR");
-        $fare_rate = env('DAY_RATE');
-        if (($current_hour >= $nightFareHour) ||
-            ($current_hour < $dayFareHour)) {
-            $fare_rate = env('NIGHT_RATE');
-        }
-        $url = "https://maps.googleapis.com/maps/api/directions/json?key=" . env('GOOGLE_API_KEY');
-        $url .= "&origin=" . $origin;
-        $url .= "&destination=" . $destination;
-        $response = Http::get($url);
+        $response = $booking->getRouteData($origin, $destination);
 
         $route_data = json_decode($response);
         $distance_metres = $route_data->routes[0]->legs[0]->distance->value;
-        $distance_kilometres = $distance_metres / 1000;
-        $fare = round($distance_kilometres * $fare_rate, 1, PHP_ROUND_HALF_UP);
+        $fare = $booking->calculateFare($booking_type, $pickup_time, $distance_metres);
 
         $travel_time = $route_data->routes[0]->legs[0]->duration->text;
         $distance_text = $route_data->routes[0]->legs[0]->distance->text;
-        // $fare = 15.00;
-        // $distance_text = "15km";
-        // $travel_time = "8 mins";
+
         $id = uniqid();
         $status = 0;
-        DB::table('booking')->insert([
-            'fcm_token' => $fcm_token,
-            'booking_id' => $id,
-            'booking_type' => $booking_type,
-            'pickup_time' => $pickup_time ? date('Y.m.d H:i:s', strtotime($pickup_time)) : "",
-            'passengers' => $passengers,
-            'origin' => $origin,
-            'destination' => $destination,
-            'fare' => $fare,
-            'status' => $status,
-            'note' => $note,
-        ]);
+        $booking->addBooking($fcm_token, $id, $booking_type, $pickup_time, $passengers,$origin, $destination, $fare, $status, $note);
 
         return response()->json(['status' => 200, 'message' => "Booking created",
             'fare' => $fare, 'distance' => $distance_text,
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Services\Booking;
 use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
  */
 
 Route::get('/', function (Booking $booking) {
-    $booking . Hi();
-    return response()->json(['message' => "Hi."]);
+    $booking->Hi("Good luck");
+    return response()->view('Welcome');
 });
